a4f2c93d92630fffa80f7d3495f24af96ead5d72
BigRodri <rodrigonnes@icloud.com>
2021-04-19 20:45:08 -0300

    NavMeshStation

---
+++ NavMeshStationStarter/Assets/Script/AIControl.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AIControl : MonoBehaviour 
+{   //criação do Agent
+    public NavMeshAgent agent;
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = this.GetComponent<NavMeshAgent>();//Pega o componente do NavMesh
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ NavMeshStationStarter/Assets/Script/AgentManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AgentManager : MonoBehaviour
+{
+    GameObject[] agents;
+    // Start is called before the first frame update
+    void Start()
+    {
+        agents = GameObject.FindGameObjectsWithTag("ai");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {   //Se o jogador clicar com o botão esquerdo do mouse os game object irão até o local
+        if (Input.GetMouseButtonDown(0)) 
+        {
+            //Gera o melhor trajeto para os game objects se moverem
+            RaycastHit hit;
+            if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 100))
+
+            {
+                foreach (GameObject a in agents)
+                    a.GetComponent<AIControl>().agent.SetDestination(hit.point);//
+            }
+        }
+
+    }
+}
